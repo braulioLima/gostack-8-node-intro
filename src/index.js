a41cb090599cf5454b04a2dc58a69841cb8f4dc1
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-01-22 13:38:37 -0300

    Feature(CRUD): CRUD proposto para o exercicio

--- src/index.js
+++ src/index.js
@@ -4,8 +4,57 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/',(req,res) => {
-  return res.send('Hello World');
+const projects = [];
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
 });
 
-server.listen(3333);
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  let project = projects.find( proj => proj.index === id);
+
+  project = {...project, title};
+
+  return res.json(project);
+
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  return res.send();
+
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  let project = projects.find( proj => proj.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+
+});
+
+server.listen(3000);
